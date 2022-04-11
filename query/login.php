392927b065fa020533a7749020cf2ea49a2a11e7
jadae007 <jadae2225@gmail.com>
2022-04-11 13:17:14 +0700

    fixbug user

--- query/login.php
+++ query/login.php
@@ -27,8 +27,9 @@ if(!empty($_POST)){
     $_SESSION['project'] = "certificate";
     if($row_user['role']== 0){
       if(isset($_POST['remember'])){
-        setcookie("role", $row_user['role'], time() +(60*60*24*7), '/birth-death-certificate/');
         setcookie("username", $row_user['username'], time() +(60*60*24*7), '/birth-death-certificate/');
+        setcookie("firstName", $row_user['firstName'], time() +(60*60*24*7), '/birth-death-certificate/');
+        setcookie("role", $row_user['role'], time() +(60*60*24*7), '/birth-death-certificate/');
       }
       $_SESSION["role"] = 0;
       $resultLogin['loginObj'][] = array("status"=>"true","role"=>$row_user['role']);
@@ -37,6 +38,7 @@ if(!empty($_POST)){
     if($row_user['role']== 1){
       if(isset($_POST['remember'])){
         setcookie("username", $row_user['username'], time() +(60*60*24*7), '/birth-death-certificate/');
+        setcookie("firstName", $row_user['firstName'], time() +(60*60*24*7), '/birth-death-certificate/');
         setcookie("role", $row_user['role'], time() +(60*60*24*7), '/birth-death-certificate/');
       }
       $_SESSION["role"] = 1;
