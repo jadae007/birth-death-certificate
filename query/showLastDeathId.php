e51d271cf40c1f3e68f5f8ea42714f1cd84bf7a0
Tanapong <jadae2225@gmail.com>
2022-12-31 22:00:28 +0700

    fix no not show when new year

--- query/showLastDeathId.php
+++ query/showLastDeathId.php
@@ -4,7 +4,11 @@ $year = $_GET['year'];
 $sql = "SELECT no FROM death WHERE no LIKE '%/$year' ORDER BY id DESC LIMIT 1";
 $result = mysqli_query($conn,$sql);
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-$no =  explode("/",$row['no']) ;
+if(mysqli_num_rows($result)>0){
+$no =  explode("/",$row['no']);
 echo $no[0]+1 . "/" . $no[1];
+}else{
+  echo "1/".$year;
+}
 mysqli_close($conn);
 ?>
